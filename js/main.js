26b31a9fdd22ea6f4e976fc9afb74f1347af855f
Karolina Wilczyńska <k.wilczynska7@gmail.com>
2017-01-28 22:04:11 +0100

    desktop js nav-bar min

--- js/main.js
+++ js/main.js
@@ -10,4 +10,26 @@ $(document).ready(function() {
         menu.toggle();
     });
 
+
+    if(window.matchMedia("(min-width: 1024px)").matches){
+
+        $(window).on("scroll", function(){
+
+            var wScroll = $(this).scrollTop();
+
+            if (wScroll > 55){
+                $("#nav-bar").css({
+                    "height": "50px"
+                })
+            }
+            else{
+                $("#nav-bar").css({
+                    "height": "115px"
+                })
+            }
+
+        });
+    }
+
+
 });
